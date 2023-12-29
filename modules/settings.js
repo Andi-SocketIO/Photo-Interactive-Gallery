843dc2c2a251f36f0fc8b4bc323c9c83ee6edeb6
Andi Waluyo <andi@antigravity.id>
2023-12-29 16:48:34 +0700

    change 5

--- modules/settings.js
+++ modules/settings.js
@@ -3,7 +3,7 @@ module.exports = {
    nodeBase_URL: "https://multiplayer-instance.andiwaluyo.com",
    onlineController_URL:
       "https://g.minigim.fun/photo-booth-controller/index.html",
-   offlineController_URL: "https://localhost:55300/index.html",
+   offlineController_URL: "https://localhost:55500/index.html",
    privateKey_PATH:
       "/etc/letsencrypt/live/multiplayer-instance.andiwaluyo.com/privkey.pem",
    certificate_PATH:
